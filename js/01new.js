7eeae8152aea9c8c01cb672d1ff03934d464227d
KaterynaBabych <at2019@ukr.net>
2022-09-27 06:17:14 +0300

    11

--- js/01new.js
+++ js/01new.js
@@ -1,19 +1,11 @@
-function makeArray(firstArray, secondArray, maxLength) {
-    // Change code below this line
 
-    const allArray = firstArray.concat(secondArray);
+const workers = 8;
+const minSalary = 500;
+const maxSalary = 2000;
+let total;
 
-    if (allArray) {
-      allArray.length > maxLength;
-      return allArray.slice(0, maxLength);
-    }
-   
-    // return newArry;
-    
-
-    // Change code above this line
-  }
-  console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3));
-  console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4));
-  console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3));
-  console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2));
+for (let i = 0; i < workers; i += 1){
+  console.log(i);
+  const mathRandom = Math.random()*((maxSalary-minSalary)+minSalary)
+  console.log(mathRandom);
+};
